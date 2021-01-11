c0fb29178a8621c73a65f67d2ba15cf9cd2727e3
multeders <multeders777@gmail.com>
2021-01-11 14:21:41 +0400

    First Commit
    First Commit

---
+++ 3DExam/Assets/cameraRotate.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotate : MonoBehaviour
+{
+    public float speed;
+    //public Camera cam;
+
+    private float minFov = 1f;
+    private float maxFov = 900f;
+    private float sensitivity = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //.transform.position = new Vector3(0, 20, 0);
+        if (Input.GetAxis("Mouse ScrollWheel") != 0.0f)
+        {
+            float fov = Camera.main.fieldOfView;
+            fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+            fov = Mathf.Clamp(fov, minFov, maxFov);
+            Camera.main.fieldOfView = fov;
+            //cam.transform.position = (cam.transform.position - gameObject.transform.position).normalized * Input.GetAxis("Mouse ScrollWheel");
+            //cam.transform.position = new Vector3(0, 0, cam.transform.position.z + Input.GetAxis("Mouse ScrollWheel"));
+        }
+        //cam.transform.position = new Vector3(0, 0, cam.transform.position.z + Input.GetAxis("Mouse ScrollWheel"));
+        //Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
+
+        if (Input.GetKey("up"))
+        {
+            transform.Rotate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.Rotate(-1 * speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, -1 * speed * Time.deltaTime, 0);
+        }
+        
+    }
+}
---
+++ 3DExam/Assets/duplicateCube.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duplicateCube : MonoBehaviour
+{
+    public Camera cam;
+    //public Transform CubeTransform;
+
+    Transform t_SelectedObject;
+    Transform t_AttachToObject;
+    float f_CubeSize = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        /* If we press right mouse button  */
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            // Debug.Log(Input.mousePosition);
+            // Debug.Log(cam.ScreenPointToRay(Input.mousePosition));
+            /* Draw ray from mouse position to check if we hit anything with certain layer */
+            //Debug.Log(Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit));
+            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                Destroy(hit.transform.gameObject);
+                //Debug.Log(hit.point);
+                /* If we hit the same object then we de-select it */
+
+                /*if (t_SelectedObject != null && t_SelectedObject == hit.transform)
+                    t_SelectedObject = null;
+                else
+                    t_SelectedObject = hit.transform;
+                    */
+            }
+        }
+
+        /* If we press left mouse button */
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Debug.Log("DOWN");
+            /* Draw ray from mouse position to check if we hit anything with certain layer */
+            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                t_AttachToObject = hit.transform;
+                Debug.Log(hit.point);
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                t_SelectedObject = cube.transform;
+                if (t_SelectedObject != null)
+                {
+                    /* Check if we do not hit the same object that we have selected */
+                    if (t_SelectedObject != hit.transform)
+                    {
+                        /* Assign position with offset so the cube center won't be at click point
+                         * Keep in mind that if you change your cube size you should change f_CubeSize as well */
+                        //t_SelectedObject.position = hit.point + hit.normal.normalized * f_CubeSize;
+
+                        /* If you need to Rotate it accordingly to object that you attach */
+                        t_SelectedObject.rotation = hit.transform.rotation;
+
+                        /* Function if you want attach cube to center of hitted object side */
+                        t_SelectedObject.position = FindSideCenter(hit.point);
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    /* Function to find right side center and return it position to attach
+     * If you don't need to attach it to center, then just remove this function */
+    Vector3 FindSideCenter(Vector3 hitPosition)
+    {
+        
+        Vector3[] sides = { t_AttachToObject.right, -t_AttachToObject.right, t_AttachToObject.forward, -t_AttachToObject.forward, t_AttachToObject.up, -t_AttachToObject.up };
+
+        float minDistance = Vector3.Distance(t_AttachToObject.position + sides[0] * f_CubeSize, hitPosition);
+        int sideIndex = 0;
+
+        for (int i = 1; i < 6; i++)
+        {
+            float curDistance = Vector3.Distance(t_AttachToObject.position + sides[i] * f_CubeSize, hitPosition);
+
+            if (curDistance < minDistance)
+            {
+                minDistance = curDistance;
+                sideIndex = i;
+            }
+        }
+
+        return t_AttachToObject.position + sides[sideIndex] * f_CubeSize;
+    }
+}
